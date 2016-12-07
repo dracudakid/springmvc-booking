957306fb824ae08a0e69fdba9f329ca15f3fabb1
Tan Dat <tandat2209@gmail.com>
2016-12-07 14:39:24 +0700

    Config to handle static files

--- src/main/java/com/mgmtp/configuration/WebConfig.java
+++ src/main/java/com/mgmtp/configuration/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -30,4 +31,11 @@ public class WebConfig extends WebMvcConfigurerAdapter{
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//                .addResourceHandler("/resources/**")
+//                .addResourceLocations("/resources/");
+//    }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -15,7 +15,8 @@
           href="<c:url value="/resources/style.css" />" >
 </head>
 <body>
-<h1>Welcome to Spittr</h1>
+<h1>Welcome to SimpleWebApp</h1>
+<div class="cirle">This is styled circle div</div>
 <a href="<c:url value="/spittles" />">Spittles</a> |
 <a href="<c:url value="/spitter/register" />">Register</a>
 </body>
